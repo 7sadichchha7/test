59c6491bb6a536d20e318bf8d9cbaa8307312e08
Sadichchha Sedhain <sadichchhasedhain@Sadichchhas-MacBook-Air.local>
2025-11-27 11:08:22 +0545

    learning python

---
+++ intro/intro/first.py
@@ -0,0 +1,12 @@
+# we want to print the patient age , name and number.
+patient_name= "Ram Thapa"
+patient_age=38
+patient_number= "9845671230"
+print("Patient Name:",patient_name)
+print("patient age:", patient_age)
+print("patient number:",patient_number)
+is_new_patient=True
+patient ={"name":"Ram Thapa", "age":38 ,"number":"9845671230"}
+print(patient)
+for val in patient.keys():
+    print(val)
